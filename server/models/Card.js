b8e9c25c881689e3f92f910a5fbcf5c6535396de
Tunnelvision77 <Tacomablues666@gmail.com>
2021-01-16 11:49:58 -0700

    Card Model Written

--- server/models/Card.js
+++ server/models/Card.js
@@ -16,6 +16,19 @@ const colorIdSchema = new Schema(
     colorIdentity: { type: String, required: true}
   })
 
+const subtypesSchema = new Schema({
+  subtypes: { type: String, required: true }
+})
+
+const printingsSchema = new Schema({
+  pritings: { type: String }
+})
+
+const legalitiesSchema = new Schema({
+  format: { type: String },
+  legality: { type: String}
+})
+
 const Card = new Schema(
   {
     name: { type: String, required: true },
@@ -23,7 +36,21 @@ const Card = new Schema(
     manaCost: { type: String, required: true },
     colors: [ colorsSchema ],
     colorIdentity: [ colorIdSchema ],
-    type: { type: String, required: true },
+    types: { type: String, required: true },
+    subtypes: [ subtypesSchema ],
+    rarity: { type: String, required: true },
+    set: { type: String },
+    setName: { type: String },
+    text: { type: String },
+    artist: { type: String },
+    number: { type: Number },
+    power: { type: Number },
+    toughness: { type: Number },
+    layout: { type: String },
+    imageUrl: { type: String },
+    printings: { printingsSchema },
+    legalities: { legalitiesSchema },
+    id: { type: String },
     creatorId: { type: String, ref: 'Account', required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
